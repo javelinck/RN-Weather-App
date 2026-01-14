542ff9925e4da7fd6560721f19bbd681e52313ff
Andrew Slota <javelinck@gmail.com>
2026-01-14 14:23:49 +0100

    feat(test): configure Jest, cover daily aggregation, format, endpoints URLs, caches

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,32 @@
 module.exports = {
-    preset: 'jest-expo',
-    setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-    transformIgnorePatterns: [
-        'node_modules/(?!(react-native|@react-native|expo|@expo|@react-native-async-storage|expo-router)/)'
-    ]
+  preset: 'jest-expo',
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  setupFiles: ['<rootDir>/jest.setup-files.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testTimeout: 15000,
+  clearMocks: true,
+  restoreMocks: true,
+  resetMocks: false,
+  transformIgnorePatterns: [
+    'node_modules/(?!(' +
+    'react-native|' +
+    '@react-native|' +
+    '@react-navigation|' +
+    'react-clone-referenced-element|' +
+    'expo-.*|' +
+    '@expo/.*|' +
+    'react-native-svg|' +
+    'lottie-react-native|' +
+    'expo-modules-core' +
+    ')/)',
+  ],
+  moduleNameMapper: {
+    '\\.(png|jpg|jpeg|gif|webp)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.svg$': '<rootDir>/__mocks__/svgMock.js',
+    '^expo($|/.*)': '<rootDir>/__mocks__/expoMock.js',
+    '^expo-constants$': '<rootDir>/__mocks__/expoConstantsMock.js',
+    '^react-native/Libraries/Animated/NativeAnimatedHelper$':
+      '<rootDir>/__mocks__/NativeAnimatedHelper.js',
+  },
 };
